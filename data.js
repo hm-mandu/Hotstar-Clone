eba2e21424068a6b13fab079f818ab0fc219b7de
hm-mandu <hritikmandlik63@gmail.com>
2022-07-06 19:25:28 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,28 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Following the events of \'Avengers: Endgame,\' Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities -- and their patience.',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of \"Avengers Endgame."',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des:'Blends the style of classic sitcoms with the MCU, in which Wanda Maximoff and Vision - two super-powered beings living their ideal suburban lives - begin to suspect that everything is not as it seems.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'In a realm known as Kumandra, a re-imagined Earth inhabited by an ancient civilization, a warrior named Raya is determined to find the last dragon.',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des:'On the Italian Riviera, an unlikely but strong friendship grows between a human being and a sea monster disguised as a human.',
+        image: 'images/slider 5.PNG'
+    },
+
+]
